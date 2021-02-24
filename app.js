132a6ccb0472a4b44a4ff376756e31cb2692ddc2
Tyler Thompson <studiesweekly@Tylers-MacBook-Pro.local>
2021-02-24 10:18:25 -0700

    changes to server for Heroku

--- app.js
+++ app.js
@@ -79,6 +79,12 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-app.listen(3013 || process.env.PORT, function() {
-  console.log("Server started on port 3013");
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3017;
+}
+
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
